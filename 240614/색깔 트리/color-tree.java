f1e831a14660cfbd0f799c9dc4881ad7e6ae7116
Jooyong Lee <jy23698@naver.com>
2024-06-14 01:36:26 +0900

    Title: 색깔 트리, Time: 1383ms, Memory: 88MB, Status: Passed - CodeTree

--- 240614/색깔 트리/color-tree.java
+++ 240614/색깔 트리/color-tree.java
@@ -9,23 +9,23 @@ public class Main {
         int color;
         int depth;
         List<Integer> children;
-        boolean[] subColor;
 
-        public Node(int myId, int parentId, int color, int depth, List<Integer> children, boolean[] subColor) {
+        public Node(int myId, int parentId, int color, int depth, List<Integer> children) {
             this.myId = myId;
             this.parentId = parentId;
             this.color = color;
             this.depth = depth;
             this.children = children;
-            this.subColor = subColor;
         }
     }
 
-    // 추가된 노드 
-    static Map<Integer, Node> nodeMap = new HashMap<>();
+    // 노드
+    static Node[] nodes = new Node[100001];
 
     // 루트
     static List<Integer> root = new ArrayList<>();
+
+    static long value;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -42,7 +42,7 @@ public class Main {
                 int p = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
                 int d = Integer.parseInt(st.nextToken());
-                Node newNode = new Node(m, p, c, d, new ArrayList<>(), new boolean[6]);
+                Node newNode = new Node(m, p, c, d, new ArrayList<>());
                 addNode(newNode);
             } else if (command == 200) {
                 int m = Integer.parseInt(st.nextToken());
@@ -52,7 +52,10 @@ public class Main {
                 int m = Integer.parseInt(st.nextToken());
                 sb.append(viewColor(m)).append("\n");
             } else if (command == 400) {
-                long value = calcValue();
+                value = 0;
+                for (Integer r : root) {
+                    subvalue(r);
+                }
                 sb.append(value).append("\n");
             }
         }
@@ -62,11 +65,10 @@ public class Main {
 
 
     static void addNode(Node newNode) {
-        // 내가 루트다 
+        // 내가 루트다
         if (newNode.parentId == -1) {
             root.add(newNode.myId);
-            nodeMap.put(newNode.myId, newNode);
-            paintColor(newNode.myId);
+            nodes[newNode.myId] = newNode;
             return;
         }
 
@@ -74,7 +76,7 @@ public class Main {
         boolean possible = true;
         // 부모한테 찾아가서 이 노드 넣어도 되는지 허락받기
         // 2부터 시작
-        Node parentNode = nodeMap.get(newNode.parentId);
+        Node parentNode = nodes[newNode.parentId];
         int d = 2;
         while (true) {
             // 지금 부모
@@ -88,120 +90,60 @@ public class Main {
             if (parentNode.parentId == -1) break;
 
             // 다음 부모 가서 허락받기
-            parentNode = nodeMap.get(parentNode.parentId);
+            parentNode = nodes[parentNode.parentId];
         }
 
         if (possible) {
-            nodeMap.put(newNode.myId, newNode);
-            nodeMap.get(newNode.parentId).children.add(newNode.myId);
-            paintColor(newNode.myId);
+            nodes[newNode.myId] = newNode;
+            nodes[newNode.parentId].children.add(newNode.myId);
         }
     }
 
     static void changeColor(int myId, int color) {
+        nodes[myId].color = color;
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(myId);
 
-        // 하향식으로 칠하고 
         while (!queue.isEmpty()) {
             Integer from = queue.poll();
-            Node currentNode = nodeMap.get(from);
-            currentNode.color = color;
-
-            // 색 단일화
-            boolean[] sc = new boolean[6];
-            sc[color] = true;
-            currentNode.subColor = sc;
-            nodeMap.put(currentNode.myId, currentNode);
-
-            for (Integer to : currentNode.children) {
+            for (Integer to : nodes[from].children) {
+                nodes[to].color = color;
                 queue.offer(to);
             }
         }
 
-        // 내 부모를 찾아가서 나의 색이 바뀌었다고 보고 
-        int pid = nodeMap.get(myId).parentId;
-
-        if (pid == -1) return;
-
-        while (true) {
-            Node parent = nodeMap.get(pid);
-            boolean[] newSub = new boolean[6];
-            newSub[parent.color] = true;
-            for (Integer c : parent.children) {
-                Node child = nodeMap.get(c);
-
-                for (int i = 1; i <= 5; i++) {
-                    if (child.subColor[i]) {
-                        newSub[i] = true;
-                    }
-                }
-            }
-            parent.subColor = newSub;
-            nodeMap.put(pid, parent);
-
-            pid = parent.parentId;
-            if (pid == -1) break;
-        }
-        
-        calcValue();
-        
     }
 
     static int viewColor(int myId) {
-        return nodeMap.get(myId).color;
+        return nodes[myId].color;
     }
 
-    static void paintColor(int myId) {
-        // 새 노드에서 타고올라가서 부모노드도 칠하기 
+    static boolean[] subvalue(int myId) {
+        boolean[] v;
 
-        Node currentNode = nodeMap.get(myId);
-
-        // 일단 나부터 칠한다
-        currentNode.subColor[currentNode.color] = true;
-        nodeMap.put(myId, currentNode);
-
-        boolean[] colored = new boolean[6];
-        colored[currentNode.color] = true;
-
-        if (currentNode.parentId == -1) return;
-
-        while (true) {
-            currentNode = nodeMap.get(currentNode.parentId);
-            colored[currentNode.color] = true;
-            for (int i = 1; i <= 5; i++) {
-                if (colored[i]) {
-                    currentNode.subColor[i] = true;
-                }
-            }    
-            nodeMap.put(currentNode.myId, currentNode);
-            if (currentNode.parentId == -1) break;
+        // 내가 자식이면 내꺼 칠해서
+        if (nodes[myId].children.size() == 0) {
+            v = new boolean[6];
+            v[nodes[myId].color] = true;
+            value += 1;
+            return v;
         }
-    }
 
-    static long calcValue() {
-        long answer = 0;
-        for (Integer r : root) {
-            Queue<Integer> queue = new LinkedList<>();
-            queue.offer(r);
-
-            while (!queue.isEmpty()) {
-                Integer fromId = queue.poll();
-                Node from = nodeMap.get(fromId);
-
-                int count = 0;
-                for (int i = 1; i <= 5; i++) {
-                    count += from.subColor[i] ? 1 : 0;
-                }
-
-                answer += count * count;
-
-                for (Integer to : from.children) {
-                    queue.offer(to);
-                }
+        v = new boolean[6];
+        v[nodes[myId].color] = true;
+        for (Integer c : nodes[myId].children) {
+            boolean[] cColor = subvalue(c);
+            for (int i = 1; i <= 5; i++) {
+                if (cColor[i]) v[i] = true;
             }
         }
-        
-        return answer;
+
+        int count = 0;
+        for (int i = 1; i <= 5; i++) {
+            if (v[i]) count++;
+        }
+
+        value += count * count;
+        return v;
     }
 }
